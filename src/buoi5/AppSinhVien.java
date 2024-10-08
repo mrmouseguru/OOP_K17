c05883f004e2c003c10363ddf22a7dd657f3633e
CGM <mrmouse.guru@gmail.com>
2024-10-08 13:51:36 +0700

    update source 13h51

--- src/buoi5/AppSinhVien.java
+++ src/buoi5/AppSinhVien.java
@@ -1,14 +1,27 @@
 package buoi5;
 
+import java.util.Scanner;
+
 public class AppSinhVien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
 		SinhVien sv1 = new SinhVien(1111, "Le Van Teo", 5.5f, 9.5f);
 		SinhVien sv2 = new SinhVien(2222, "Le Van Ty", 7.5f, 5.5f);
 		
 		System.out.println(sv1);
-		System.out.print(sv2);
+		System.out.println(sv2);
+		//Sinh vien 3
+		SinhVien sv3 = new SinhVien();
+		System.out.print("Nhap ma so SV:");
+		sv3.maSV = input.nextInt();
+		input.nextLine();
+		System.out.print("Nhap ten SV:");
+		sv3.hoTen = input.nextLine();
+		
+		System.out.println(sv3);
 
 	}
 
